fb6140e77e97f1e98d776bef9c86d0ad1caaed3c
Tobias Fried <friedtm@gmail.com>
2019-09-17 17:25:43 -0400

    Cleaned up Geolocation request

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
-import { PermissionsAndroid } from 'react-native';
-import MetaWeather from "../api/MetaWeather";
+import {useEffect, useState} from 'react';
+import {PermissionsAndroid} from 'react-native';
+import MetaWeather from '../api/MetaWeather';
 import Geolocation from '@react-native-community/geolocation';
 
 export default () => {
-  const [location, setLocation] = useState(null)
+  const [location, setLocation] = useState(null);
   const [results, setResults] = useState([]);
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
 
   /**
    * Poll the MetaWeather API for weather info based on device location or user query
@@ -19,59 +19,34 @@ export default () => {
     if (query) {
       params.query = query;
       makeRequest(params);
-    } else {  
+    } else {
       getLocationAsync();
-    } 
-    
+    }
   };
-
 
   const makeRequest = async params => {
     try {
-      const response = await MetaWeather.get("/api/location/search", { params });
+      const response = await MetaWeather.get('/api/location/search', {params});
       setResults(response.data);
     } catch (err) {
-      setError("Could not connect to MetaWeather");
+      setError('Could not connect to MetaWeather');
     }
-  }
-
-  const getLocationAsync = async () => {
-    try {
-
-      // Request permission to get the device location
-      let permitted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, 
-        {
-          title: 'Request Location Permission',
-          message:
-            'iX Weather would like to use your location ' +
-            'to provide weather data for nearby cities.',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        });
-
-      // Get permission if on iOS  
-      if (!permitted === PermissionsAndroid.RESULTS.GRANTED) {
-        Geolocation.requestAuthorization();
-        permitted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
-        if (!permitted === PermissionsAndroid.RESULTS.GRANTED) {
-          setError('Permission to access location was denied');
-          return;
-        }
-      }
-      
-      // Retrieve the decive coordinates make the API call with them
-      Geolocation.getCurrentPosition(info => {
-        let coords = info.coords;
-        setLocation(coords);
-        makeRequest({ lattlong: `${coords.latitude},${coords.longitude}` });
-      });
-    } catch (err) {
-      console.warn(err);
-    } 
-
   };
-  
+
+  // Retrieve the device coordinates make the API call with them
+  const getLocationAsync = async () => {
+    // Requests location permission if not already granted
+    Geolocation.getCurrentPosition(info => {
+      let coords = info.coords;
+      setLocation(coords);
+      makeRequest({lattlong: `${coords.latitude},${coords.longitude}`});
+    }, () => {
+      // On permission denied, make a query with default location
+      // setError("Location permission was denied. Try making a search.");
+      makeRequest({ lattlong: '0,0' });
+    });
+  };
+
   // Get device location once when the component is created.
   useEffect(() => {
     getLocationAsync();
