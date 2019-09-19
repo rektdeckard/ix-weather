a5676c831402c397190374d4fab96a5d9f91ab8d
Tobias Fried <friedtm@gmail.com>
2019-09-19 15:14:30 -0400

    Delete old metaWeather API

--- src/api/metaWeather.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://www.metaweather.com",
-});
-
-export const getImageUri = weatherSate => {
-  return 'https://www.metaweather.com/static/img/weather/png/' + weatherSate + '.png'
-};
